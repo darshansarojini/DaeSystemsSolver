d3e44f9b6abb76545e7850526acad10040b96a6b
zehuang@ucsd.edu <zoey@zoey-vpc>
2022-11-28 02:27:29 -0800

    minor change in code

--- CSDL/Test/PEND_Experiment_CSDL.py
+++ CSDL/Test/PEND_Experiment_CSDL.py
@@ -7,22 +7,22 @@ Created on Wed Sep 21 11:39:27 2022
 
 import sys
 sys.path.append('..')
-from Model.PEND_Model_CSDL import SMD_R,SMD_F
+from Model.PEND_Model_CSDL import PEND_R,PEND_F
 from ForSol.Time_Marching_CSDL import Newton
 from AdSol.Adjoint_Solver_CSDL import Adjoint
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-sim_R = SMD_R()
-sim_F = SMD_F()
+sim_R = PEND_R()
+sim_F = PEND_F()
 x0 = np.array([[0,3]])
-xDot0 = np.array([[0,0]])
+xDot0 = np.array([[3,0]])
 t_initial = 0
 t_final = 10
 time_step = 10**(-3)
 tolerance = 10**(-8)
-mu = ['l']
+mu = ['l','b','m']
 
 import timeit
 start = timeit.default_timer()
@@ -33,8 +33,9 @@ stop = timeit.default_timer()
 print('Time: ', stop - start) 
 
 plt.plot(experiment.time,experiment.x_hist[:,0])
-plt.title('Pendulum Angle')
+plt.plot(experiment.time,experiment.x_hist[:,1])
+plt.title('Angular Displacement (rad) and Angular Velocity (rad/s)')
 plt.xlabel('time (s)')
-plt.ylabel('angle (rad)')
+plt.ylabel('Displacement (rad) and Velocity(rad/s)')
 plt.grid(True)
 plt.show()
